48664a0be3c5f1dd0a78c622baf55898e4c407ab
Aaron Dyke <adyke@ADyke-MacBookPro.local>
2018-01-12 11:19:28 -0500

    Menu page Done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,7 @@ export class HomePage {
     { title: 'Our Menu', pic: 'assets/img/soup1.jpg', pushPage: 'MenuPage' },
     { title: 'Account', pic: 'assets/img/coffee-people3.jpg', pushPage: 'AcountPage' },
     { title: 'About Us', pic: 'assets/img/coffee6.jpg', pushPage: 'AboutPage' },
-    { title: 'Locations', pic: 'assests/img/cafe2.jpg', pushpage: 'LocationsPage' }
+    { title: 'Locations', pic: 'assets/img/cafe2.jpg', pushpage: 'LocationsPage' }
   ]
   constructor(public navCtrl: NavController) {
 
