16cfdbc8bdb6a071d9aeebe24601a4c6390d9da5
Aaron Dyke <adyke@ADyke-MacBookPro.local>
2018-01-12 11:41:03 -0500

    Configuring User Registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey : "AIzaSyCBPnRnP5mq6OzBx9Ds4VU-CAWRGjDZD50",
+  authDomain: "wiredbarin.firebaseapp.com",
+  databaseURL: "https://wiredbarin.firebaseio.com",
+  storageBucket: "wiredbarin.appspot.com",
+  messagingSenderId: '602355310311'
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
