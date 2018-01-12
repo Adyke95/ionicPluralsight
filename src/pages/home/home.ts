d860a833a5e092aa14006e8bf3b89ea7cec5d803
Aaron Dyke <adyke@ADyke-MacBookPro.local>
2018-01-12 11:08:31 -0500

    Added Theming and Started Navigation Menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  menuData = [
+    { title: 'Our Menu', pic: 'assets/img/soup1.jpg', pushPage: 'MenuPage' },
+    { title: 'Account', pic: 'assets/img/coffee-people3.jpg', pushPage: 'AcountPage' },
+    { title: 'About Us', pic: 'assets/img/coffee6.jpg', pushPage: 'AboutPage' },
+    { title: 'Locations', pic: 'assests/img/cafe2.jpg', pushpage: 'LocationsPage' }
+  ]
   constructor(public navCtrl: NavController) {
 
   }
